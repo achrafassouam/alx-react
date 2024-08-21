29d9db1a926ef5a87798fa0b1ec5177a2551a92a
Achraf Assouam <achrafassouam@gmail.com>
2024-08-22 00:25:35 +0100

    adding task_5

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from './Notifications/Notifications';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Notifications />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
