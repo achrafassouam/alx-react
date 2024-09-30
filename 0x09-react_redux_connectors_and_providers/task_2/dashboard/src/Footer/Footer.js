c0513b347a9ab63fbaf829257df88b046c382a29
Achraf Assouam <achrafassouam@gmail.com>
2024-09-30 21:53:55 +0100

    Task_9. Connect user state to the Footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,36 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
 import { getFullYear, getFooterCopy } from "../utils/utils";
-import { StyleSheet, css } from "aphrodite";
-import { AppContext } from "../App/AppContext";
+import PropTypes from "prop-types";
 
-
-const styles = StyleSheet.create({
-  paragraph: {
-    textAlign: 'center',
-    fontSize: '1.2rem'
-  },
-  copyright: {
-    marginTop: '40px'
-  }
-})
-
-function Footer() {
+export function Footer({ user }) {
   return (
-    <AppContext.Consumer>
-      {(context) => (
-        <footer className='footer'>
-          <p className={css(styles.copyright)}>
-            Copyright {getFullYear()} - {getFooterCopy(true)}
-          </p>
-          {context.user.isLoggedIn && (
-            <p className={css(styles.paragraph)}>
-              <a>Contact us</a>
-            </p>
-          )}
-        </footer>
-      )}
-    </AppContext.Consumer>
+    <div className="footer">
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user && <a href="#">Contact us</a>}
+    </div>
   );
 }
 
-export default Footer;
+Footer.defaultProps = {
+  user: null,
+};
+
+Footer.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"),
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
